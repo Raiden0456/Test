7df7ac181d5c191c01ee6070efd495e4901a39ca
Vrezh Fedora <vrezhraiden@gmail.com>
2022-10-21 21:12:34 +0200

    connected database

--- DB.php
+++ DB.php
@@ -1,18 +1,13 @@
 <?php 
-class DB 
-{
-    
-}
-$host = "127.0.0.1";
-$user = "root";
-$password = "Nani1995";
-$database = "Test_1";
-$connect = new mysqli($host,$user,$password,$database);
 
-public function getBySql($sql, $connect)
+function getBySql($sql, $connect)
 {
     $return = $connect->query($sql);
+    if ($return)
+        return $return;
+    else 
+        return 'Ошибка соединения';
 }
-    
+
 
 ?>
--- index.php
+++ index.php
@@ -10,9 +10,42 @@
 </head>
 <body>
     <?php 
-    //Getting data from MySql//
-    
-
+        include 'DB.php';
+        $host = "127.0.0.1";
+        $user = "root";
+        $password = "Nani1995";
+        $database = "Test_1";
+        $connect = new mysqli($host,$user,$password,$database);
+        
+        //Getting products from MySql//
+        $sql = "SELECT * FROM `products`";
+        $result = getBySql($sql,$connect);
+        while($obj = mysqli_fetch_object($result))
+        {
+            $result_obj = new stdClass();
+            $result_obj->id = $obj->id;
+            $result_obj->cost = $obj->price;
+            
+            switch($obj->description)
+            {
+                case (str_contains($obj->description, "звонки")):
+                    $result_obj->image =  "phone";
+                    break;
+                case (str_contains($obj->description, "спецкурс")):
+                    $result_obj->image =  "book";
+                    break;
+                default:
+                    $result_obj->image =  "book2";
+                    break;
+            }
+            $dis_des = explode( "%", $obj->description);
+            $result_obj->red_discount = $dis_des[0]."%";
+            $result_obj->text = $dis_des[1];
+            
+            $products[] = $result_obj;
+        }
+       
+        $result -> free_result();
     
     ?>
     <div class = "boxes">
@@ -41,29 +74,23 @@
             <div class = "discounts_container">
 
                 <?php 
-                    $item_1 = array("cost" => "50", "image" => "phone", "red_discount" => "50%", "text" => "на звонки ST (x2)", "button" => true);
-                    $item_2 = array("cost" => "70", "image" => "book", "red_discount" => "30%", "text" => "на спецкурс", "button" => false);
-                    $item_3 = array("cost" => "100", "image" => "book2", "red_discount" => "50%", "text" => "на курс", "button" => false);
-                    $item_4 = array("cost" => "139", "image" => "book2", "red_discount" => "65%", "text" => "на курс", "button" => false);
-                    
-                    $data = array($item_1, $item_2, $item_3, $item_4);
-                    foreach($data as $item): 
+                    foreach($products as $item): 
                 ?>
                     <div class = "discount">
                         <div>
                             <img src = "assets/images/Монета ET 3.svg" class = "ET_2_photo">
-                            <?= $item["cost"] ?>
+                            <?= $item->cost ?>
                             <img src = "assets/images/ET.png" class = "ET_2">
                         </div>
                         <div>
-                            <img class = "discounts_image" src = "assets/images/<?=$item["image"]?>.png">
-                            <h class = "text_phone"> <?=$item["image"] == "phone" ? "x2" : "" ?> </h>
+                            <img class = "discounts_image" src = "assets/images/<?=$item->image?>.png">
+                            <h class = "text_phone"> <?=$item->image == "phone" ? "x2" : "" ?> </h>
                         </div>
                         <div class = "description">
                             <div class = "red_text">
-                                <?=$item["red_discount"]?>
+                                <?=$item->red_discount?>
                             </div>
-                            <?=$item["text"]?>
+                            <?=$item->text?>
                         </div>
                         <button class = "buttton_state" onclick="">
                             Купить
